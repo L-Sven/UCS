4ff1765017de1251780b605884404bf4061d4d30
simon johansson <simonjohansson92@live.se>
2018-04-09 13:35:10 +0200

    La till leverantörsfakturor huvud och rader. Även lagt till ny data i kundfakturorna.

---
+++ UCSTest/UCSTest/UCSTest/KundFakturaRad.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UCSTest
+{
+    class KundFakturaRad
+    {
+
+        public string ArtikelNummer { get; set; } = "";
+        public string Benämning { get; set; } = "";
+        public double LevAntal { get; set; }
+        public string EnhetsTyp { get; set; } = "";
+        public double StyckPris { get; set; }
+        public double TotalKostnad { get; set; }
+
+
+    }
+}
---
+++ UCSTest/UCSTest/UCSTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Adk = AdkNetWrapper;
+using System.Windows;
+
+
+namespace UCSTest
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+            VismaData go = new VismaData();
+        }
+
+        
+           
+    }
+}
