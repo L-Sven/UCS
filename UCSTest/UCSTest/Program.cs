e505996edf99f4aca7e0934bafd83909e8c8da47
simon johansson <simonjohansson92@live.se>
2018-04-23 16:05:12 +0200

    Don't mind this commit, lul

--- UCSTest/UCSTest/Program.cs
+++ UCSTest/UCSTest/Program.cs
@@ -15,6 +15,20 @@ namespace UCSTest
 
         static void Main(string[] args)
         {
+            /*
+              IF EXISTS (SELECT @dokumentNummer FROM Avtal where dokumentNummer = @dokumentNummer)
+	BEGIN
+		UPDATE Avtal SET dokumentNummer = @dokumentNummer, avtalsDatum = @avtalsDatum, startDatum = @startDatum, tempSlutDatum = @tempSlutDatum,
+		kundNummer = @kundnummer, isActive = @isActive, fakturaIntervall = @fakturaIntervall, periodStart = @periodStart, periodEnd = @periodEnd,
+		totalKostnad = @totalKostnad, uppsägningstid = @uppsägningstid, förlängningstid = @förlängningstid, avtalsDatumSlut = @avtalsDatumSlut
+	END
+	ELSE
+	BEGIN
+	INSERT INTO Avtal (dokumentNummer, avtalsDatum, startDatum, tempSlutDatum, kundNummer, isActive, fakturaIntervall, periodStart, periodEnd, totalKostnad, uppsägningstid, förlängningstid, avtalsDatumSlut)
+	VALUES
+	(@dokumentNummer, @avtalsDatum, @startDatum, @tempSlutDatum, @kundnummer, @isActive, @fakturaIntervall, @periodStart, @periodEnd, @totalKostnad, @uppsägningstid, @förlängningstid, @avtalsDatumSlut)
+END
+             */
 
             VismaData go = new VismaData();
             // C:\users\sijoh0500\Work Folders\Documents\Github\UCSTest\UCSTest\fakturaDB.mdf
