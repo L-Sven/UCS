4ff1d6c3511c55192f15aabae99a80b5d80d0830
Sven <sven.lubbe.syne17@tucsweden.se>
2018-05-07 10:46:53 +0200

    tuk

--- UCSTest/UCSTest/Program.cs
+++ UCSTest/UCSTest/Program.cs
@@ -21,41 +21,9 @@ namespace UCSTest
             string ftg = ConfigurationManager.AppSettings["ftgPath"];
             string sys = ConfigurationManager.AppSettings["sysPath"];
             string startDatum = ConfigurationManager.AppSettings["datum"];
-
-            //ChooseVismaFilePath();
+            
             //VismaTidData vismaTid = new VismaTidData(ftg, sys);
             VismaData go = new VismaData(ftg, sys, startDatum);
-            // C:\users\sijoh0500\Work Folders\Documents\Github\UCSTest\UCSTest\fakturaDB.mdf
         }
-
-       /* private static void ChooseVismaFilePath()
-        {
-            while (ftg.Length == 0 || sys.Length == 0)
-            {
-                FolderBrowserDialog selectFolder = new FolderBrowserDialog();
-                selectFolder.SelectedPath = @"C:\ProgramData\SPCS\SPCS Administration";
-                selectFolder.Description = "Vänligen ange sökvägen till Företaget som ska öppnas.";
-                if (selectFolder.ShowDialog() == DialogResult.OK)
-                {
-                    ftg = selectFolder.SelectedPath;
-                }
-                selectFolder.Dispose();
-
-                FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
-                folderBrowser.SelectedPath = @"C:\ProgramData\SPCS\SPCS Administration";
-                folderBrowser.Description = "Vänligen ange sökvägen till Visma Administrations gemensamma filer.";
-                if (folderBrowser.ShowDialog() == DialogResult.OK)
-                {
-                    sys = folderBrowser.SelectedPath;
-                }
-
-                folderBrowser.Dispose();
-            }
-            
-
-
-
-        } */
-
     }
 }
